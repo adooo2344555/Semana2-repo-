b90e4b5d56cb9e6166e25b57f989341f984c5235
adooo2344555 <brayanadonaypenatealas@gmail.com>
2026-03-22 16:16:19 -0600

    Ejercicios con string Ejercicio1 Phyton

---
+++ EjerciciosSemana9Py/Ejercicio1.py
@@ -0,0 +1,16 @@
+# Ejercicio 1
+
+# Pedimos el nombre completo
+nombre = input("Ingrese su nombre completo: ")
+
+# MAYUSCULAS
+nombre_mayusculas = nombre.upper()
+print(nombre_mayusculas)
+
+# minusculas
+nombre_minusculas = nombre.lower()
+print(nombre_minusculas)
+
+# cantidad 
+cantidad = len(nombre)
+print(cantidad)
