55b30dada103cf33d2221e46e7efef5ff3c629f8
adooo2344555 <brayanadonaypenatealas@gmail.com>
2026-03-22 19:52:39 -0600

    Ejercicios de clases que todavia no habia subido de semana 7 y 9

--- EjerciciosPython/RepasoSemana9.py
+++ EjerciciosPython/RepasoSemana9.py
@@ -3,23 +3,23 @@ Serie = "fullmetal alchemits"
 ### cada variable tiene un espacio de memoria asignado
 
 ## cuando una vable cambia => se pierde la inmutabilidad
-## POO 
-## polimorfismo -> es el cambio de acciones sin que se rompa el codigo 
-## abstracciones -> 
-##      Tasa de cafe   
-#       cafe coscafe 
-##       azucar 
-##      agua 
+## POO
+## polimorfismo -> es el cambio de acciones sin que se rompa el codigo
+## abstracciones ->
+##      Tasa de cafe
+#       cafe coscafe
+##       azucar
+##      agua
 ##      otros ingredientes
 ##  un objeto es el que toma un modelo y este modelo le da funciones y utiliza sus propiedades
 
-## Leon -> 
+## Leon ->
 ## tiene ojos  (propiedades)
-## tiene boca 
-## Esta guapo 
+## tiene boca
+## Esta guapo
 #############
 #  corre        ( Funciones )
-# salta 
+# salta
 
 ## Clases
 ## Estructura de datos.
@@ -44,18 +44,34 @@ def saludo(nombres):
 
 ## Colocar el nombre de la serie como titulo
 fmaTemu = Serie.title()
-saludo(Serie)
-saludo(fmaTemu)
+# saludo(Serie)
+# saludo(fmaTemu)
+fnaMayusculas = Serie.upper()
+saludo(fnaMayusculas)
 ## deprogracion Lineal
+FullmetalCapitalizer = fnaMayusculas.swapcase().title()
+## cuando encadenamos funciones se indica que la salidad de la funcion actual
+# 3 es la entrada de la siguiente funcion.
 
+saludo(FullmetalCapitalizer)
 
 ## compara cadenas de texo
-nombre = "Brayan Adonay Penate"
-password = "1234"
+comparar1 = "Ever"
+comparar2 = "Ever"
 
-if nombre == "Brayan Adonay Penate":
-    contra = str(input("Ingrese su pass: "))
-    if password == contra:
-        print("Wellcome")
+# casefold para comparar y pasar a  minusculas
+variableTemporal = comparar2.casefold()
+comparar = comparar1.casefold() == comparar2.casefold()
+# print(comparar)
+## casefold nos dara true unicamente si los elementos son identicos sino nos indcara un false
 
+## para verificar si es nu nuemero o un caracter vamos a utilizar isalfa()
+clasicas2005 = "Gasolina"
+comprarisAlpha = clasicas2005.isalpha()
+#print(comprarisAlpha, 2005)
+#isalpha nos va a dar true si el string que se le esta enviando es unicamente letras
 
+##si lo que quiero es que sea solo numero isalnum
+
+letraCancion = "Lo que paso, paso entre tu y yo" #no tiene numeros
+decada = 10
