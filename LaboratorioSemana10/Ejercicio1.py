50c921dde6d6898d31ac3b1b08fca42569dd3cc0
adooo2344555 <brayanadonaypenatealas@gmail.com>
2026-04-08 09:47:18 -0600

    Ejercicios de laboratorio de la Semana10 Ejercicio1

---
+++ LaboratorioSemana10/Ejercicio1.py
@@ -0,0 +1,16 @@
+def transformar(texto, numero):
+    if numero == 1:
+        print(texto.upper())
+    elif numero == 2:
+        print(texto.lower())
+    elif numero == 3:
+        print(texto.capitalize())
+    else:
+        print("Opción incorrecta")
+
+
+texto = input("Escriba un texto: ")
+numero = int(input("Ingrese un número (1, 2 o 3): "))
+
+
+transformar(texto, numero)
