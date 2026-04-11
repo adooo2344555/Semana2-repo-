553e2dfc7f890e1ab9b1e4157b8f5054e0167176
adooo2344555 <brayanadonaypenatealas@gmail.com>
2026-04-11 01:24:43 -0600

    Parcial 02 Fundamentos de programación, ejercicio 8 uso de count y splitlines en bloque de texto

---
+++ Parcial02/Ejercicio8.py
@@ -0,0 +1,11 @@
+texto1 = """Por una mirada, un mundo;
+por una sonrisa, un cielo;
+por un beso... ¡yo no sé
+qué te diera por un beso"""
+
+cantidad = texto1.count("a")
+
+lineas = texto1.splitlines()
+
+print(cantidad)
+print(lineas)
