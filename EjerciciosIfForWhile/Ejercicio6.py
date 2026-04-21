561b0dc5d67659f2016651ac6fc3397399248dd1
adooo2344555 <brayanadonaypenatealas@gmail.com>
2026-04-21 01:06:51 -0600

    Ejercicio6 Semana12

---
+++ EjerciciosIfForWhile/Ejercicio6.py
@@ -0,0 +1,19 @@
+
+number = int(input("Ingrese un número del 1 al 7: "))
+
+if number == 1:
+    print("Lunes")
+elif number == 2:
+    print("Martes")
+elif number == 3:
+    print("Miércoles")
+elif number == 4:
+    print("Jueves")
+elif number == 5:
+    print("Viernes")
+elif number == 6:
+    print("Sábado")
+elif number == 7:
+    print("Domingo")
+else:
+    print("Error: número fuera de rango")
