4aa914b589da4ae81e966dc94a7aadfa290536a8
adooo2344555 <brayanadonaypenatealas@gmail.com>
2026-04-21 00:59:51 -0600

    Ejercicio1 Semana12

---
+++ EjerciciosIfForWhile/Ejercicio1.py
@@ -0,0 +1,9 @@
+
+number = float(input("Ingrese un número: "))
+
+if number > 0:
+    print("El número es positivo")
+elif number < 0:
+    print("El número es negativo")
+else:
+    print("El número es cero")
